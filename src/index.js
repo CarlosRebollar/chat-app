b11f2dcb05441c2cd0e173c06ae1ef756cc0f6f5
Carlos Palacio <rebo@Carloss-iMac.local>
2019-06-03 20:07:22 -0500

    Sharing location and disabling button until server sends acknowledge

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketIO = require("socket.io");
+const Filter = require("bad-words");
 
 const app = express();
 const server = http.createServer(app);
@@ -20,8 +21,20 @@ io.on("connection", socket => {
     socket.emit("message", "Welcome!");
     socket.broadcast.emit("message", "A new user has joined!");
 
-    socket.on("sendMessage", (message) => {
+    socket.on("sendMessage", (message, cb) => {
+        const filter = new Filter();
+
+        if (filter.isProfane(message)) {
+            return cb("Profanity is not allowed")
+        }
+
         io.emit("message", message);
+        cb();
+    });
+
+    socket.on("sendLocation", ({ latitude, longitude }, cb) => {
+        io.emit("message", `https://google.com/maps?q=${latitude},${longitude}`)
+        cb();
     });
 
     socket.on("disconnect", () => {
